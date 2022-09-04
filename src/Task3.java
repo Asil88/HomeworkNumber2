cf072589c0510bf8b06495bb4bf7f1bc8aeedfe3
Anton Lisovski <lisovskijj.99@gmail.com>
2022-09-04 13:00:26 +0300

    ДЗ дополнительное 3 задача.

---
+++ src/Task3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+        System.out.println(Math.sqrt((Math.pow(x,2))+(Math.pow(y,2))));
+
+    }
+}
+
+
