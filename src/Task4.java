f4d87df7ca1bde4a9d55ec5f1ed75ad802afa197
Anton Lisovski <lisovskijj.99@gmail.com>
2022-09-04 13:03:10 +0300

    ДЗ дополнительное 4 задача.

---
+++ src/Task4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(Math.sqrt((Math.pow(x, 2)) + (Math.pow(y, 2))));
+
+
+    }
+}
