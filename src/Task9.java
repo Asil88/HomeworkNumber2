44a6911363bf7e775f79c35ec7b72665df3956f6
Anton Lisovski <lisovskijj.99@gmail.com>
2022-09-04 21:08:28 +0300

    ДЗ дополнительное 9 задача.

---
+++ src/Task9.java
@@ -0,0 +1,16 @@
+public class Task9 {
+    public static void main(String[] args) {
+        int x=5,y=7,z=0,k=9;
+
+
+        z=x*x+y*y;
+        System.out.println(z);
+
+        z= (int)Math.pow(x,3);
+        System.out.println(z);
+
+        k=x/z;
+        System.out.println(k);
+
+    }
+}
