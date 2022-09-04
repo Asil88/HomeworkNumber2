07a05993c6e5c3abcb601369dea2652399da0fc6
Anton Lisovski <lisovskijj.99@gmail.com>
2022-09-04 21:00:11 +0300

    ДЗ дополнительное 8 задача.

---
+++ src/Task8.java
@@ -0,0 +1,7 @@
+public class Task8 {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+        long z=2147483647+1L; // Переменные типа int могут принимать значения:-2147483648 до 2147483647
+        System.out.println(z);
+    }
+}
