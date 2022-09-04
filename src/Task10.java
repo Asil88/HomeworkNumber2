1177bc24e15c5f4cb87cb725379533d3fff551d9
Anton Lisovski <lisovskijj.99@gmail.com>
2022-09-04 23:55:24 +0300

    ДЗ дополнительное 10 задача.

---
+++ src/Task10.java
@@ -0,0 +1,18 @@
+public class Task10 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        int d = 3;
+        int i = 4;
+        int e = 5;
+        int z = 6;
+        int x = 7;
+        int v = 8;
+        int m = 9;
+        int sum =(a+b+c+d+i+e+z+x+v+m)/10;
+        System.out.println(sum);
+        double sumd=(a+b+c+d+i+e+z+x+v+m)/10;
+        System.out.println(sumd==sum);
+    }
+}
